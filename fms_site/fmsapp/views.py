e20da409bd44eba7daa9ce8a309e9a89f677e498
mbaseman1918 <39381570+mbaseman1918@users.noreply.github.com>
2018-08-24 14:13:31 -0700

    got the map to display fires in the form of a heatmap

--- fms_site/fmsapp/views.py
+++ fms_site/fmsapp/views.py
@@ -17,34 +17,27 @@ def index(request):
         }
     return render(request,'fmsapp/index.html', context)
 
-# def add_address(request):
-#     if request.method == "POST":
-#         new_user = request.user
-#         new_street_address = request.POST['street_address']
-#         new_city = request.POST['city']
-#         new_state = request.POST['state']
-#         new_address = Address(user=new_user, street_address=new_street_address, city=new_city, state=new_state)
-#         new_address.save()
-#     return HttpResponseRedirect(reverse('fmsapp:index'))
 
-def delete_address(request):
-    pass
 
 def compare_address(request):
     pass
 
 def display_address_on_map(request):
-    lat_lng = get_user_lat_lng(request)
-    lat = lat_lng['lat']
-    lng = lat_lng['lng']
-    print(lat_lng)
-    print(lat)
-    print(lng)
-    # return HttpResponse(lat_lng['lat'])
-    map_src = "https://maps.googleapis.com/maps/api/js?key="+GeoCoding_API+"&callback=initMap"
+    fire_list = get_fire_data()
+    fire_lat = []
+    fire_lng = []
+    for i in fire_list[1:]:
+        fire_lat.append(float(i['lat']))
+        fire_lng.append(float(i['lng']))
+    user_lat_lng = get_user_lat_lng(request)
+    user_lat = user_lat_lng['lat']
+    user_lng = user_lat_lng['lng']
+    map_src = "https://maps.googleapis.com/maps/api/js?key="+GeoCoding_API+"&libraries=visualization&callback=initMap"
     context = {
     'map_user':map_src,
-    'lat':lat,
-    'lng':lng,
+    'user_lat':user_lat,
+    'user_lng':user_lng,
+    'fire_lat':fire_lat,
+    'fire_lng':fire_lng,
     }
     return render(request, 'fmsapp/map.html', context)
--- fms_site/utilities/geocoding.py
+++ fms_site/utilities/geocoding.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 
 def get_user_lat_lng(request):
-    current_user = get_object_or_404(User, username=request.user.username)
+    current_user = get_object_or_404(User, username='newuser')
     targ_address = get_object_or_404(Address, user=current_user)
     physical_address = targ_address.street_address+',+'+targ_address.city+',+'+targ_address.state
     url = 'https://maps.googleapis.com/maps/api/geocode/json?address='+physical_address+'&key='+GeoCoding_API
